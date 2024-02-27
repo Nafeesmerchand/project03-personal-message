24a8d86a39f9a4c5a8d2f9ef0cdc40723b92ed68
Nafeesmerchand <160695676+Nafeesmerchand@users.noreply.github.com>
2024-02-27 07:37:22 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,11 @@
+let personname :string = '';
+
+personname = prompt('what is your name?') || '';
+if (personname !== null && personname !==''){
+alert(`Hello ${personname} would you like to learn some python today?`)
+}
+else {
+    alert ("you have to fill your name!")
+
+}
+
